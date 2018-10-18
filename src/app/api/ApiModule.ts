8246786996b28d12027ba65b018a8661a4b318a0
codeforges <ruben@codeforges.com>
2018-10-18 22:35:49 +0200

    Move api config to ApiModule

--- src/app/api/ApiModule.ts
+++ src/app/api/ApiModule.ts
@@ -2,12 +2,19 @@ import {Global, Module} from "@nestjs/common";
 import {SvandisKamiApiModule} from "./svnadis-api/v1/svandis-kami-api/SvandisKamiApiModule";
 import {StatisticsController} from "./svnadis-api/v2/statisctics/controllers/StatisticsController";
 import {SvandisWorkerApiModule} from "./svnadis-api/v2/SvandisWorkerApiModule";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Global()
 @Module({
     imports: [
         SvandisKamiApiModule,
-        SvandisWorkerApiModule
+        SvandisWorkerApiModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            url: process.env.PG_CONNECTION_URL || '',
+            ssl: true,
+            entities: ['src/**/**.entity{.ts,.js}']
+        })
     ],
     controllers: [
         StatisticsController
